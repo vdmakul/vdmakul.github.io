b133c5178baca77daeac8c89d58877c7a7fee729
Vadim Akula <vadim.akula@corp.mail.ru>
2016-11-06 21:55:16 +0200

    dynamics on welcome page selected

--- js/behobby.js
+++ js/behobby.js
@@ -37,9 +37,10 @@ function demo_want_teach_select() {
 }
 
 function demo_run_hobby() {
-    if($('#want-hobby-link img').hasClass('welcome_selected')) {
+    if ($('#want-hobby-link img').hasClass('welcome_selected')) {
         demo_open_search_with_results();
-    } else {
+    }
+    if ($('#want-teach-link img').hasClass('welcome_selected')) {
         demo_open_create_hobby();
     }
 }
@@ -58,4 +59,31 @@ function demo_open_hobby() {
 
 function demo_open_create_hobby() {
     $('#create-hobby-link').click();
+}
+
+function on_interests_select() {
+    $('#selected-interests').append(
+        '<span class="badge">' + $('#interests-select').val() +
+        '<button type="button" onclick="delete_interest(event)" class="close"><span>&times;</span></button>' +
+        '</span>');
+    $('#interests-select').val('');
+}
+
+function delete_interest(event) {
+    $(event.target).closest('span.badge').remove();
+}
+
+function toggle_final_step() {
+    var final = false;
+    if ($('#want-hobby-link img').hasClass('welcome_selected')) {
+        final = $('#selected-interests').find('span.badge').length > 0
+            || $('#select-personal-panel').find('input[name=gender]:checked').length > 0
+            || $('#select-personal-panel').find('input[name=age]').val() != "";
+    }
+    if ($('#want-teach-link img').hasClass('welcome_selected')) {
+        final = $('#intro-teacher-panel').find('input[name=nameSurname]').val() != "" &&
+            $('#intro-teacher-panel').find('input[name=email]').val() != "";
+    }
+
+    $('#final-step img').toggleClass("behobby_selected", final)
 }
